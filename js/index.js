6440b996b25f3da2d41890c49fb77d8d2c06b9be
ykassou <ykassou.91@gmail.com>
2025-05-06 11:19:38 +0200

    Ajout max caracteres

--- js/index.js
+++ js/index.js
@@ -6,3 +6,15 @@ function basculerMotdepasse() {
   champ.type = estCache ? 'text' : 'password';
   icone.textContent = estCache ? '🙃' : '👀';
 }
+function mettreAJourCompteur(nomID, compteurID) {
+  const input = document.getElementById(nomID);
+  const compteur = document.getElementById(compteurID);
+  const maxLength = input.maxLength;
+
+  input.addEventListener('input', () => {
+    const longueur = input.value.length;
+    compteur.textContent = `${longueur} / ${maxLength} caractères`;
+  });
+}
+mettreAJourCompteur("email", "compteur-email");
+mettreAJourCompteur("motdepasse", "compteur-mdp");
