b425be0b718696b3977ac70ec57cccaa3f839050
Valérie ROUX <30691126+valerierx@users.noreply.github.com>
2025-05-06 18:02:34 +0200

    feat: résolution voyages

--- profil.php
+++ profil.php
@@ -47,27 +47,46 @@ if (!isset($_SESSION['connecte'])) {
             <div class="profil-elmt">
                 <img src="media/profil.jpg" alt="Photo de Profil" class="avatar">
                 <?php
-                if (isset($_GET["succes"])) {
-                    echo '<p class="message valide">
-                Votre profil a été modifié!
-                </p>';
+                if (isset($_GET["mail"])) {
+                    echo '<p class="message erreur">
+            L\'adresse mail est déjà utilisée!
+            </p>';
                 }
                 ?>
-                <h1>Profil</h1>
-                <p>Nom: <?= $_SESSION['nom'] ?></p>
-                <p>Prénom: <?= $_SESSION['prenom'] ?></p>
-                <p>Email: <?= $_SESSION['mail'] ?></p>
-                <p>Date de naissance : <?= $_SESSION['naissance'] ?></p>
-                <h2>Adresse</h2>
-                <p><?= $_SESSION['numero'] ?> <?= $_SESSION['rue'] ?></p>
-                <?php
-                if (isset($_SESSION["complement"])) {
-                    echo '<p>' . $_SESSION['complement'] . '</p>';
-                }
-                ?>
-                <p><?= $_SESSION['codePostal'] ?> <?= $_SESSION['commune'] ?></p>
+                <form action="api/profil.php" method="POST">
+                    <h1>Informations personnelles</h1>
+                    <label for="nom">
+                        <input type="text" autocomplete="off" placeholder="Nom" name="nom"
+                               value="<?php echo $_SESSION['nom']; ?>" required>
+                    </label>
+                    <label>
+                        <input type="text" placeholder="Prénom" name="prenom" value="<?php echo $_SESSION['prenom']; ?>"
+                               required>
+                    </label>
+                    <label>
+                        <input type="date" placeholder="Date de naissance" name="naissance" <?php
+                        if ($_SESSION["naissance"] != "1879-10-26") {
+                            echo 'value="' . $_SESSION["naissance"] . '"';
+                        } ?> required>
+                    </label>
+                    <h3>Adresse postale</h3>
+                    <div class="rue">
+                        <input type="number" autocomplete="off" placeholder="N°" name="numero"
+                               value="<?php echo $_SESSION['numero']; ?>">
+                        <input type="text" placeholder="Rue" name="rue" value="<?php echo $_SESSION['rue']; ?>" required>
+                    </div>
+                    <input type="text" placeholder="Complément d'adresse" name="complement"
+                           value="<?php echo $_SESSION['complement']; ?>">
+                    <div class="rue">
+                        <input type="number" autocomplete="off" placeholder="Code postal" name="codePostal"
+                               value="<?php echo $_SESSION['codePostal']; ?>" required>
+                        <input type="text" placeholder="Commune" name="commune"
+                               value="<?php echo $_SESSION['commune']; ?>" required>
+                    </div>
+                    <!--<select name="commune" id="commune"></select>-->
+                    <button class="modif-btn" type="submit">Modifier le profil</button>
+                </form>
                 <div class="bouton-array">
-                    <button class="modif-btn"><a href="modifProfil.php">Modifier Profil</a></button>
                     <?php if ($_SESSION['role'] == 0) {
                         echo '<button class="admin-btn"><a href="admin.php">Tableau de bord admin</a></button>';
                     }; ?>
